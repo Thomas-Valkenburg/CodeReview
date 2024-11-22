2f9d72bed87b5186be1496419db98e12dc86f85f
Thomas V <36117329+Thomas-Valkenburg@users.noreply.github.com>
2024-11-22 17:35:46 +0100

    Dev (#8)
    * Add class diagram
    
    * Migrate database

---
+++ DAL/Migrations/20241122160810_Add CreatedAt and Likes to Post.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtandLikestoPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Likes",
+                table: "Posts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Likes",
+                table: "Posts");
+        }
+    }
+}
