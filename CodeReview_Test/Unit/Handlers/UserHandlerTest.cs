09455a49477928d6aa34be400f64478f592f25f0
Thomas V <36117329+Thomas-Valkenburg@users.noreply.github.com>
2024-11-28 10:47:14 +0100

    Add unit tests & Backend Handlers (#11)
    * Dev (#10)
    
    * Add class diagram
    
    * Migrate database
    
    * Move AccountUser to Core.Models
    
    * Update dependencies, refactor, and add new tests
    
    - Updated `CodeReview_Test.csproj` to include `Moq` v4.20.72 and removed an unnecessary `ItemGroup`.
    - Added `using Core.Handlers` in `ServiceProviderHandler.cs` and registered `UserHandler` as a transient service.
    - Changed return type of `CreateUser` in `UserHandler.cs` from `Result<User>` to `Result`.
    - Refactored `Result` class methods in `Result.cs` and updated `Result<T>` to allow nullable `Value`.
    - Added `UserHandlerTest` class in `UserHandlerTest.cs` with a test for `CreateUser` method.

---
+++ CodeReview_Test/Unit/Handlers/UserHandlerTest.cs
@@ -0,0 +1,44 @@
+﻿using Core.Handlers;
+using Core.Interfaces;
+using Core.Models;
+using Moq;
+
+namespace CodeReview_Test.Unit.Handlers;
+
+internal class UserHandlerTest
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void UserHandler_CreateUser_NoError()
+    {
+        var mock = new Mock<IUserService>();
+        mock.Setup(x => x.Create(It.IsAny<User>())).Verifiable();
+
+        var userHandler = new UserHandler(mock.Object);
+
+        Assert.DoesNotThrow(() =>
+        {
+            var user = new User();
+            userHandler.CreateUser(user);
+        });
+    }
+
+    /*[Test]
+    public void UserHandler_GetUser_NoError()
+    {
+        var mock = new Mock<IUserService>();
+        mock.Setup(x => x.GetById(It.IsAny<int>())).Returns(new User()).Verifiable();
+
+        var userHandler = new UserHandler(mock.Object);
+
+        Assert.DoesNotThrow(() =>
+        {
+            userHandler.GetUser(1);
+        });
+    }*/
+}
--- Core/Result.cs
+++ Core/Result.cs
@@ -5,18 +5,14 @@ public class Result(bool success, string? message)
     public bool Success { get; init; }
     public string Message { get; init; }
 
-    public static Result<T> FromSuccess<T>(T obj, string? message = null)
-    {
-        return new Result<T>(obj, true, message);
-    }
+    public static Result FromSuccess(string? message = null) => new(true, message);
+    public static Result<T> FromSuccess<T>(T obj, string? message = null) => new(obj, true, message);
 
-    public static Result FromException(string? message = null)
-    {
-        return new Result(false, message);
-    }
+    public static Result FromException(string? message = null) => new(false, message);
+    public static Result<T> FromException<T>(string? message = null) => new(default, false, message);
 }
 
-public class Result<T>(T obj, bool success, string? message) : Result(success, message)
+public class Result<T>(T? obj, bool success, string? message) : Result(success, message)
 {
-    public T Value { get; init; } = obj;
+    public T? Value { get; init; } = obj;
 }
