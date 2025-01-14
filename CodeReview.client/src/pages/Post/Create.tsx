8bfc4ca6cf7ddab882e54ec07151e654ca42bcaa
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2025-01-14 23:27:39 +0100

    Redirect to login page if unauthorized

--- CodeReview.client/src/pages/Post/Create.tsx
+++ CodeReview.client/src/pages/Post/Create.tsx
@@ -1,6 +1,26 @@
+import { useState, useEffect } from "react"
 import PostForm from "../../components/NewFolder/PostForm";
 
 function create() {
+    const [email, setEmail] = useState<String>();
+
+    async function populateAccountInformation() {
+        await fetch("/manage/info", {
+            credentials: "include"
+        })
+        .then(async (response) => {
+            // ReSharper disable once TsResolvedFromInaccessibleModule
+            if (response.status === 401) {
+                window.location.href = "/account/login";
+            }
+        })
+        .catch((error) => console.log(error));
+    }
+
+    useEffect(() => {
+        populateAccountInformation();
+    }, []);
+
     return (
         <div>
             <PostForm />
