5b7b9df3fcd79fded9f55990855815b7d3905f09
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2025-01-15 00:48:05 +0100

    add cypress end-to-end/UI tests

---
+++ CodeReview.client/cypress/e2e/test.cy.js
@@ -0,0 +1,102 @@
+describe('User stories', () => {
+  const email = "email@email.com";
+  const password = "Password123!";
+
+  const title = "Cypress Test title";
+
+  function login() {
+    const url = cy.url();
+
+    cy.visit('/account/login');
+
+    cy.get('.flex-grow-1 > .d-flex > :nth-child(1) > .form-control').type(email);
+    cy.get('.flex-grow-1 > .d-flex > :nth-child(2) > .form-control').type(password);
+
+    cy.get('.flex-row > .flex-grow-1 > .d-flex > .btn').click();
+
+    cy.wait(1000);
+
+    cy.visit(url.href ?? '/');
+  }
+
+  function logout() {
+    const url = cy.url();
+  
+    try {
+      cy.visit('/');
+
+      cy.get('.dropdown > .d-none').click();
+      cy.get(':nth-child(2) > .dropdown-item').click();
+      
+      cy.wait(1000);
+
+      cy.visit(url.href ?? '/');
+    } catch (e) {
+      cy.visit(url.href ?? '/');
+    }
+  }
+
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('Register', () => {
+    cy.get('[href="/account/register"]').click();
+
+    cy.url().should('include', '/account/register');
+
+    cy.get('.flex-grow-1 > .d-flex > :nth-child(1) > .form-control').type(email);
+    cy.get('.flex-grow-1 > .d-flex > :nth-child(2) > .form-control').type(password);
+
+    cy.get('.flex-row > .flex-grow-1 > .d-flex > .btn').click();
+  });
+
+  it('Login', () => {
+    login();
+
+    cy.get('.dropdown > .d-none').click();
+
+    cy.get(':nth-child(1) > .dropdown-item').should('have.text', email);
+    cy.get(':nth-child(2) > .dropdown-item').should('have.text', 'Logout');
+  });
+
+  it("Create Post", () => {
+    login();
+
+    cy.get('.flex-column > :nth-child(1) > :nth-child(2) > .btn').click();
+
+    cy.get('.m-auto > .form-control').type(title);
+
+    cy.get('.notranslate').type("Cypress Test content");
+
+    cy.get('.m-auto > .btn-primary').click();
+
+    cy.get('#search-bar').type(title);
+
+    cy.get('.d-sm-block > .d-flex > .btn').click();
+
+    cy.get('[href="/post/28"] > .card-body > .card-title').should('have.text', title);
+  });
+
+  it ("View Post", () => {
+    cy.get('[href="/post/28"]').click();
+
+    cy.get('h2').should('have.text', title);
+
+    cy.get('.public-DraftStyleDefault-block > [data-offset-key="dnra-0-0"] > span').should('have.text', 'Cypress Test content');
+  });
+
+  it("Comment on Post", () => {
+    login();
+
+    cy.get('[href="/post/28"]').click();
+
+    cy.get('.notranslate').type("Cypress Test comment");
+
+    cy.get('.flex-column > .btn-primary').click();
+
+    cy.visit(window.location.href);
+
+    cy.get('.public-DraftStyleDefault-block > [data-offset-key="evhds-0-0"] > span').should('have.text', 'Cypress Test comment');
+  });
+});
